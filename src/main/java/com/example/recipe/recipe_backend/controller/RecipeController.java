dc80ac895f1ed25b2a75d1b670c7471a9ecfee9f
Payam <payamnamin@programmer.net>
2025-01-29 01:20:26 +0100

    create a method on Controller to calling the API

--- src/main/java/com/example/recipe/recipe_backend/controller/RecipeController.java
+++ src/main/java/com/example/recipe/recipe_backend/controller/RecipeController.java
@@ -2,6 +2,8 @@ package com.example.recipe.recipe_backend.controller;
 
 import com.example.recipe.recipe_backend.model.Recipe;
 import com.example.recipe.recipe_backend.repository.RecipeRepository;
+import com.example.recipe.recipe_backend.service.MealService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,4 +33,17 @@ public class RecipeController {
     public void deleteRecipe(@PathVariable Long id) {
         recipeRepository.deleteById(id);
     }
+
+    @Autowired
+    private MealService mealService;
+
+    // Search for a meal by name using the Meal Database API
+    @GetMapping("/search/{mealName}")
+    public String searchMeal(@PathVariable String mealName) {
+        return mealService.searchMeal(mealName);
+  }
+
+    
+
+    
 }
